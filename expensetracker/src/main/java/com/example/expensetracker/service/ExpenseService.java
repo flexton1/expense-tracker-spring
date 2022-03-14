3b44e5f561e547aaf8746a128e6cd8705f2e36e8
HARUN JASAREVIC <harunjasarevic00@gmail.com>
2022-03-15 00:26:02 +0100

    commit

--- expensetracker/src/main/java/com/example/expensetracker/service/ExpenseService.java
+++ expensetracker/src/main/java/com/example/expensetracker/service/ExpenseService.java
@@ -8,4 +8,13 @@ public interface ExpenseService {
     
 
    List<Expense> findAll();
+
+
+   Expense save(Expense expense);
+
+
+   Expense findById(long id);
+
+   void delete(long id);
+   
 }
--- expensetracker/src/main/java/com/example/expensetracker/service/ExpenseServiceImpl.java
+++ expensetracker/src/main/java/com/example/expensetracker/service/ExpenseServiceImpl.java
@@ -2,8 +2,6 @@ package com.example.expensetracker.service;
 import java.util.List;
 import com.example.expensetracker.model.Expense;
 import com.example.expensetracker.repository.ExpenseRepository;
-import com.example.expensetracker.service.ExpenseService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +16,30 @@ public class ExpenseServiceImpl implements ExpenseService {
 @Override   
 public List<Expense> findAll(){
    return expenseRepository.findAll();
+}
+
+
+@Override
+public Expense save(Expense expense) {
+    expenseRepository.save(expense);
+    return expense;
+}
+
+
+@Override
+public Expense findById(long id) {
+    if(expenseRepository.findById(id).isPresent()){
+        return expenseRepository.findById(id).get();
+    }
+return null;
+
+}
+
+
+@Override
+public void delete(long id) {
+    Expense expense = findById(id);
+    expenseRepository.delete(expense);
 }
 
 }
